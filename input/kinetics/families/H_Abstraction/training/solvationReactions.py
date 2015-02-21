807eaea46cbc30bdf516861af16aca7326afadb9
Belinda Slakman <slakman.b@husky.neu.edu>
2015-02-21 14:55:53 -0500

    Added training barrier corrections and a dictionary for hydrogen abstraction.
    Similar to training reactions for gas-phase. This is just for solvent=water.

--- input/kinetics/families/H_Abstraction/training/solvationReactions.py
+++ input/kinetics/families/H_Abstraction/training/solvationReactions.py
@@ -8,8 +8,9 @@ longDesc = u"""
 entry(
     index = 1,
     reactants = ['CH3', 'CH4'], 
+    products = ['CH4', 'CH3'],
     solvent = 'water',
-    barrierCorrection = (-4.4, 'kJ/mol'),
+    correction = BarrierCorrection(correction = (-4.40530739996, 'kJ/mol')),
     shortDesc = u"""MO6-2X/MG3S calculations in g09 with SMD solvation model""",
     longDesc = 
 u"""
@@ -19,8 +20,9 @@ u"""
 entry(
     index = 2,
     reactants = ['OH', 'CH4'], 
+    products = ['H2O', 'CH3'],
     solvent = 'water',
-    barrierCorrection = (-3.2, 'kJ/mol'),
+    correction = BarrierCorrection(correction = (-3.2434, 'kJ/mol')),
     shortDesc = u"""MO6-2X/MG3S calculations in g09 with SMD solvation model""",
     longDesc = 
 u"""
@@ -30,11 +32,395 @@ u"""
 entry(
     index = 3,
     reactants = ['OOH', 'CH4'],
+    products = ['H2O2', 'CH3'],
     solvent = 'water',
-    barrierCorrection = (-2.6, 'kJ/mol'),
+    correction = BarrierCorrection(correction = (-2.634, 'kJ/mol')),
     shortDesc = u"""MO6-2X/MG3S calculations in g09 with SMD solvation model""",
     longDesc = 
 u"""
 """
 )
 
+entry(
+    index = 4,
+    reactants = ['CH3', 'C2H6'],
+    products = ['CH4', 'C2H5'],
+    solvent = 'water',
+    correction = BarrierCorrection(correction = (-5.12703646, 'kJ/mol')),
+    shortDesc = u"""MO6-2X/MG3S calculations in g09 with SMD solvation model""",
+    longDesc = 
+u"""
+"""
+)
+
+entry(
+    index = 5,
+    reactants = ['OH', 'C2H6'], 
+    products = ['H2O', 'C2H5'],
+    solvent = 'water',
+    correction = BarrierCorrection(correction = (-2.739934860052, 'kJ/mol')),
+    shortDesc = u"""MO6-2X/MG3S calculations in g09 with SMD solvation model""",
+    longDesc = 
+u"""
+"""
+)
+
+entry(
+    index = 6,
+    reactants = ['OOH', 'C2H6'], 
+    products = ['H2O2', 'C2H5'],
+    solvent = 'water',
+    correction = BarrierCorrection(correction = (-3.535, 'kJ/mol')),
+    shortDesc = u"""MO6-2X/MG3S calculations in g09 with SMD solvation model""",
+    longDesc = 
+u"""
+"""
+)
+
+entry(
+    index = 7,
+    reactants = ['CH3', 'C3H8'], 
+    products = ['CH4', 'C3H7'],
+    solvent = 'water',
+    correction = BarrierCorrection(correction = (-5.01, 'kJ/mol')),
+    shortDesc = u"""MO6-2X/MG3S calculations in g09 with SMD solvation model""",
+    longDesc = 
+u"""
+"""
+)
+
+entry(
+    index = 8,
+    reactants = ['OH', 'C3H8'], 
+    products = ['H2O', 'C3H7'],
+    solvent = 'water',
+    correction = BarrierCorrection(correction = (-3.151, 'kJ/mol')),
+    shortDesc = u"""MO6-2X/MG3S calculations in g09 with SMD solvation model""",
+    longDesc = 
+u"""
+"""
+)
+
+entry(
+    index = 9,
+    reactants = ['CH3', 'CH3OH_C'],
+    products = ['CH4', 'CH2OH'],
+    solvent = 'water',
+    correction = BarrierCorrection(correction = (11.305, 'kJ/mol')),
+    shortDesc = u"""MO6-2X/MG3S calculations in g09 with SMD solvation model""",
+    longDesc = 
+u"""
+"""
+)
+
+entry(
+    index = 10,
+    reactants = ['OH', 'CH3OH_C'],
+    products = ['H2O', 'CH2OH'],
+    solvent = 'water',
+    correction = BarrierCorrection(correction = (3.598, 'kJ/mol')),
+    shortDesc = u"""MO6-2X/MG3S calculations in g09 with SMD solvation model""",
+    longDesc = 
+u"""
+"""
+)
+
+entry(
+    index = 11,
+    reactants = ['OOH', 'CH3OH_C'],
+    products = ['H2O2', 'CH2OH'],
+    solvent = 'water',
+    correction = BarrierCorrection(correction = (-0.738, 'kJ/mol')),
+    shortDesc = u"""MO6-2X/MG3S calculations in g09 with SMD solvation model""",
+    longDesc = 
+u"""
+"""
+)
+
+entry(
+    index = 12,
+    reactants = ['CH3', 'CH3OH_O'],
+    products = ['CH4', 'CH3O'],
+    solvent = 'water',
+    correction = BarrierCorrection(correction = (14.262048579948, 'kJ/mol')),
+    shortDesc = u"""MO6-2X/MG3S calculations in g09 with SMD solvation model""",
+    longDesc = 
+u"""
+"""
+)
+
+entry(
+    index = 13,
+    reactants = ['OH', 'CH3OH_O'],
+    products = ['H2O', 'CH3O'],
+    solvent = 'water',
+    correction = BarrierCorrection(correction = (-7.559, 'kJ/mol')),
+    shortDesc = u"""MO6-2X/MG3S calculations in g09 with SMD solvation model""",
+    longDesc = 
+u"""
+"""
+)
+
+entry(
+    index = 14,
+    reactants = ['OOH', 'CH3OH_O'],
+    products = ['H2O2', 'CH3O'],
+    solvent = 'water',
+    correction = BarrierCorrection(correction = (18.041, 'kJ/mol')),
+    shortDesc = u"""MO6-2X/MG3S calculations in g09 with SMD solvation model""",
+    longDesc = 
+u"""
+"""
+)
+
+
+entry(
+    index = 15,
+    reactants = ['CH3', 'C2H5OH_sec'],
+    products = ['CH4', 'C2H4OH_sec'],
+    solvent = 'water',
+    correction = BarrierCorrection(correction = (-4.107, 'kJ/mol')),
+    shortDesc = u"""MO6-2X/MG3S calculations in g09 with SMD solvation model""",
+    longDesc = 
+u"""
+"""
+)
+
+entry(
+    index = 16,
+    reactants = ['OH', 'C2H5OH_sec'],
+    products = ['H2O', 'C2H4OH_sec'],
+    solvent = 'water',
+    correction = BarrierCorrection(correction = (-0.278, 'kJ/mol')),
+    shortDesc = u"""MO6-2X/MG3S calculations in g09 with SMD solvation model""",
+    longDesc = 
+u"""
+"""
+)
+
+entry(
+    index = 17,
+    reactants = ['OOH', 'C2H5OH_sec'],
+    products = ['H2O2', 'C2H4OH_sec'],
+    solvent = 'water',
+    correction = BarrierCorrection(correction = (1.266, 'kJ/mol')),
+    shortDesc = u"""MO6-2X/MG3S calculations in g09 with SMD solvation model""",
+    longDesc = 
+u"""
+"""
+)
+
+entry(
+    index = 18,
+    reactants = ['CH3', 'C2H5OH_prim'],
+    products = ['CH4', 'C2H4OH_prim'],
+    solvent = 'water',
+    correction = BarrierCorrection(correction = (-2.098, 'kJ/mol')),
+    shortDesc = u"""MO6-2X/MG3S calculations in g09 with SMD solvation model""",
+    longDesc = 
+u"""
+"""
+)
+
+entry(
+    index = 19,
+    reactants = ['OH', 'C2H5OH_prim'],
+    products = ['H2O', 'C2H4OH_prim'],
+    solvent = 'water',
+    correction = BarrierCorrection(correction = (4.997, 'kJ/mol')),
+    shortDesc = u"""MO6-2X/MG3S calculations in g09 with SMD solvation model""",
+    longDesc = 
+u"""
+"""
+)
+
+entry(
+    index = 20,
+    reactants = ['OOH', 'C2H5OH_prim'],
+    products = ['H2O2', 'C2H4OH_prim'],
+    solvent = 'water',
+    correction = BarrierCorrection(correction = (1.336, 'kJ/mol')),
+    shortDesc = u"""MO6-2X/MG3S calculations in g09 with SMD solvation model""",
+    longDesc = 
+u"""
+"""
+)
+
+entry(
+    index = 21,
+    reactants = ['CH3', 'C2H5OH_O'],
+    products = ['CH4', 'C2H5O'],
+    solvent = 'water',
+    correction = BarrierCorrection(correction = (2.08, 'kJ/mol')),
+    shortDesc = u"""MO6-2X/MG3S calculations in g09 with SMD solvation model""",
+    longDesc = 
+u"""
+"""
+)
+
+entry(
+    index = 22,
+    reactants = ['OH', 'C2H5OH_O'],
+    products = ['H2O', 'C2H5O'],
+    solvent = 'water',
+    correction = BarrierCorrection(correction = (-6.034, 'kJ/mol')),
+    shortDesc = u"""MO6-2X/MG3S calculations in g09 with SMD solvation model""",
+    longDesc = 
+u"""
+"""
+)
+
+entry(
+    index = 23,
+    reactants = ['OOH', 'C2H5OH_O'],
+    products = ['H2O2', 'C2H5O'],
+    solvent = 'water',
+    correction = BarrierCorrection(correction = (14.704, 'kJ/mol')),
+    shortDesc = u"""MO6-2X/MG3S calculations in g09 with SMD solvation model""",
+    longDesc = 
+u"""
+"""
+)
+
+entry(
+    index = 24,
+    reactants = ['CH3', 'C3H7OH_tert'],
+    products = ['CH4', 'C3H6OH_tert'],
+    solvent = 'water',
+    correction = BarrierCorrection(correction = (-4.35, 'kJ/mol')),
+    shortDesc = u"""MO6-2X/MG3S calculations in g09 with SMD solvation model""",
+    longDesc = 
+u"""
+"""
+)
+
+entry(
+    index = 25,
+    reactants = ['OH', 'C3H7OH_tert'],
+    products = ['H2O', 'C3H6OH_tert'],
+    solvent = 'water',
+    correction = BarrierCorrection(correction = (0.755, 'kJ/mol')),
+    shortdesc = u"""mo6-2x/mg3s calculations in g09 with smd solvation model""",
+    longdesc = 
+u"""
+"""
+)
+
+entry(
+    index = 26,
+    reactants = ['OOH', 'C3H7OH_tert'],
+    products = ['H2O2', 'C3H6OH_tert'],
+    solvent = 'water',
+    correction = BarrierCorrection(correction = (-1.662, 'kJ/mol')),
+    shortdesc = u"""mo6-2x/mg3s calculations in g09 with smd solvation model""",
+    longdesc = 
+u"""
+"""
+)
+
+entry(
+    index = 27,
+    reactants = ['CH3', 'C3H7OH_sec'],
+    products = ['CH4', 'C3H6OH_sec'],
+    solvent = 'water',
+    correction = BarrierCorrection(correction = (9.258, 'kJ/mol')),
+    shortdesc = u"""mo6-2x/mg3s calculations in g09 with smd solvation model""",
+    longdesc = 
+u"""
+"""
+)
+
+entry(
+    index = 28,
+    reactants = ['OH', 'C3H7OH_sec'],
+    products = ['H2O', 'C3H6OH_sec'],
+    solvent = 'water',
+    correction = BarrierCorrection(correction = (-0.046, 'kJ/mol')),
+    shortdesc = u"""mo6-2x/mg3s calculations in g09 with smd solvation model""",
+    longdesc = 
+u"""
+"""
+)
+
+entry(
+    index = 29,
+    reactants = ['OOH', 'C3H7OH_sec'],
+    products = ['H2O2', 'C3H6OH_sec'],
+    solvent = 'water',
+    correction = BarrierCorrection(correction = (1.2419, 'kJ/mol')),
+    shortdesc = u"""M06-2x/MG3S calculations in g09 with SMD solvation model""",
+    longdesc = 
+u"""
+"""
+)
+
+entry(
+    index = 30,
+    reactants = ['CH3', 'C3H7OH_prim'],
+    products = ['CH4', 'C3H6OH_prim'],
+    solvent = 'water',
+    correction = BarrierCorrection(correction = (10.184, 'kJ/mol')),
+    shortdesc = u"""M06-2x/MG3S calculations in g09 with SMD solvation model""",
+    longdesc = 
+u"""
+"""
+)
+
+entry(
+    index = 31,
+    reactants = ['OH', 'C3H7OH_prim'],
+    products = ['H2O', 'C3H6OH_prim'],
+    solvent = 'water',
+    correction = BarrierCorrection(correction = (6.039, 'kJ/mol')),
+    shortdesc = u"""M06-2x/MG3S calculations in g09 with SMD solvation model""",
+    longdesc = 
+u"""
+"""
+)
+
+entry(
+    index = 32,
+    reactants = ['OOH', 'C3H7OH_prim'],
+    products = ['H2O2', 'C3H6OH_prim'],
+    solvent = 'water',
+    correction = BarrierCorrection(correction = (2.874, 'kJ/mol')),
+    shortdesc = u"""M06-2x/MG3S calculations in g09 with SMD solvation model""",
+    longdesc = 
+u"""
+"""
+)
+
+entry(
+    index = 33,
+    reactants = ['CH3', 'C3H7OH_O'],
+    products = ['CH4', 'C3H7O'],
+    solvent = 'water',
+    correction = BarrierCorrection(correction = (14.772, 'kJ/mol')),
+    shortdesc = u"""M06-2x/MG3S calculations in g09 with SMD solvation model""",
+    longdesc = 
+u"""
+"""
+)
+
+entry(
+    index = 34,
+    reactants = ['OH', 'C3H7OH_O'],
+    products = ['H2O', 'C3H7O'],
+    solvent = 'water',
+    correction = BarrierCorrection(correction = (19.638, 'kJ/mol')),
+    shortdesc = u"""M06-2x/MG3S calculations in g09 with SMD solvation model""",
+    longdesc = 
+u"""
+"""
+)
+
+entry(
+    index = 35,
+    reactants = ['OOH', 'C3H7OH_O'],
+    products = ['H2O2', 'C3H7O'],
+    solvent = 'water',
+    correction = BarrierCorrection(correction = (15.613, 'kJ/mol')),
+    shortdesc = u"""M06-2x/MG3S calculations in g09 with SMD solvation model""",
+    longdesc = 
+u"""
+""
