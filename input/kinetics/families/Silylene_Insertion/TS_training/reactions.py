d2de4c6f26e0abbece3a772e5f4888b7fc5dddd3
Pierre Bhoorasingh <bhoorasingh.p@husky.neu.edu>
2015-10-22 01:01:26 -0400

    Add TS training data for Silylene insertion

---
+++ input/kinetics/families/Silylene_Insertion/TS_training/reactions.py
@@ -0,0 +1,59 @@
+entry(
+    index = 1,
+    label = "H2 + H2Si <=> H4Si",
+    degeneracy = 1,
+    distances = DistanceData(
+        distances = {'d12': 1.14234, 'd13': 1.63115, 'd23': 1.52108},
+        method = 'm062x/mg3s',
+    ),
+    rank = 3,
+    shortDesc = u"""Manual M06-2X/MG3S calculation.""",
+)
+
+entry(
+    index = 2,
+    label = "H2 + H4Si2 <=> H6Si2",
+    degeneracy = 1,
+    distances = DistanceData(
+        distances = {'d12': 1.11196, 'd13': 1.62865, 'd23': 1.54411},
+        method = 'm062x/mg3s',
+    ),
+    rank = 3,
+    shortDesc = u"""Manual M06-2X/MG3S calculation.""",
+)
+
+entry(
+    index = 3,
+    label = "H2 + H6Si3 <=> H8Si3",
+    degeneracy = 1,
+    distances = DistanceData(
+        distances = {'d12': 1.03580, 'd13': 1.63907, 'd23': 1.56433},
+        method = 'm062x/mg3s',
+    ),
+    rank = 3,
+    shortDesc = u"""Manual M06-2X/MG3S calculation.""",
+)
+
+entry(
+    index = 4,
+    label = "CHN + H2Si <=> CH3NSi",
+    degeneracy = 1,
+    distances = DistanceData(
+        distances = {'d12': 1.34749, 'd13': 1.87265, 'd23': 1.69676},
+        method = 'm062x/mg3s',
+    ),
+    rank = 3,
+    shortDesc = u"""Manual M06-2X/MG3S calculation.""",
+)
+
+entry(
+    index = 5,
+    label = "ClHSi + H2 <=> ClH3Si",
+    degeneracy = 1,
+    distances = DistanceData(
+        distances = {'d12': 1.19488, 'd13': 1.64983, 'd23': 1.50694},
+        method = 'm062x/mg3s',
+    ),
+    rank = 3,
+    shortDesc = u"""Manual M06-2X/MG3S calculation.""",
+)
